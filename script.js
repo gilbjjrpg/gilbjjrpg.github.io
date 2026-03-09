28c55efcc333eb180718e676508b1c0ee131bc85
gilbjjrpg <gilbjj@farmingdale.edu>
2026-03-09 00:33:21 -0400

    Project 1 Website 3/9/26 12:32AM
    More changes. Cursor changes to pointer on clickable images. Clicking the X, outside the image or pressing the Esc key should close the image.

--- script.js
+++ script.js
@@ -5,21 +5,39 @@ images.forEach(img => {
         const popup = document.createElement("div");
         popup.classList.add("popup");
 
+        const imageBox = document.createElement("div");
+        imageBox.classList.add("image-box");
+
         const bigImage = document.createElement("img");
         bigImage.src = img.src;
 
-        popup.appendChild(bigImage);
+        const closeBtn = document.createElement("span");
+        closeBtn.innerHTML = "&times;";
+        closeBtn.classList.add("close-btn");
+
+        imageBox.appendChild(bigImage);
+        popup.appendChild(closeBtn);
+        popup.appendChild(imageBox);
         document.body.appendChild(popup);
 
         popup.addEventListener("click", () => {
             popup.remove();
         });
 
+        imageBox.addEventListener("click", (e) => {
+            e.stopPropagation();
+        });
+
+        closeBtn.addEventListener("click", (e) => {
+            e.stopPropagation();
+            popup.remove();
+        });
+
         document.addEventListener("keydown", function escClose(event) {
-            if(event.key === "Escape") {
+            if (event.key === "Escape") {
                 popup.remove();
                 document.removeEventListener("keydown", escClose);
             }
-        })
+        });
     });
 });
